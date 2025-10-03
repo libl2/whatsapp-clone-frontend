01becf826f7eef04da305572855b2195d175a959
libl2 <ahngh07@gmail.com>
2025-10-03 04:11:09 +0300

    lisaning to media rady

--- src/components/message-item/message.item.jsx
+++ src/components/message-item/message.item.jsx
@@ -1,11 +1,37 @@
-import React from "react";
 import Wrapper from "./message.item.style";
 import TailinIcon from "../icons/tailin.icon";
 import moment from "moment";
+import { socket } from "../../services/socket.service";
+import { useState, useEffect } from "react";
 
 const MessageItem = ({ msg, chat }) => {
+  const [mediaUrl, setMediaUrl] = useState(msg.mediaUrl);
   const classes = ["message-conatainer", msg.fromMe ? "me" : ""];
   const time = moment(msg.timestamp * 1000).format("HH:mm");
+
+  useEffect(() => {
+    if (msg.hasMedia) {
+      // האזנה לאירוע media-ready
+      const handleMediaReady = (data) => {
+        if (data.messageId === msg.id._serialized) {
+          setMediaUrl(data.mediaUrl);
+        }
+      };
+
+      socket.on('media-ready', handleMediaReady);
+
+      // ניקוי המאזין כשהקומפוננטה מתפרקת
+      return () => {
+        socket.off('media-ready', handleMediaReady);
+      };
+    }
+  }, [msg.id._serialized]);
+  
+  if (msg.hasMedia) {
+    console.log('Media URL:', msg.mediaUrl);
+    console.log('Message type:', msg.type);
+  }
+
   return (
     <Wrapper>
       <div className={classes.join(" ")}>
@@ -16,6 +42,12 @@ const MessageItem = ({ msg, chat }) => {
           <div className="sender">{msg.from}</div>
           <div className="message">
             <div className="text">{msg.body}</div>
+            {msg.hasMedia && (
+              <div className="media">
+                {msg.type === "image" && mediaUrl && <img src={mediaUrl} alt="Media" />}
+                {msg.type === "video" && mediaUrl && <video src={mediaUrl} controls />}
+              </div>
+            )}
             <div className="time">{time}</div>
           </div>
         </div>
